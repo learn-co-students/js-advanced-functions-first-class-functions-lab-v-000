89f5153428d57abf3268c513bb9065c2efe31e36
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2017-09-20 00:13:14 -0400

    adding solution

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-<<<<<<< HEAD
-let mostProfitableNeighborhood = 'Chelsea'
-const companyName = 'Scuber'
-let companyCeo = 'Susan Smith'
-=======
->>>>>>> master
+const returnFirstTwoDrivers = (drivers) => {
+  return drivers.slice(0, 2)
+}
+
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(drivers.length - 2, drivers.length)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplyValue) {
+  return function (value) {
+    return multiplyValue * value;
+  };
+}
+
+function selectDifferentDrivers(drivers, whichDrivers){
+  return whichDrivers(drivers)
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
