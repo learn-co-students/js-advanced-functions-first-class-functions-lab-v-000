3ac907c3ad1a8d0375fa02149ad7abe4c088a9b1
Malind Kowalski <malind.nikulski@gmail.com>
2017-10-03 14:36:42 -0600

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  const twoDrivers = [drivers[0], drivers[1]]
+  return twoDrivers
+}
 
-// const returnFirstTwoDrivers = anonymous function(arrayOfDrivers) {
-  // return driver1, driver2 (technically driver 0 & 1)
-    //maybe const twoDrivers = driver1 & driver2 selected
-    //return twoDrivers
-// }
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2)
+}
 
-//const returnLastTwoDrivers = anonymous function(arrayOfDrivers) {
-  //return arrayOfDrivers.slice(-2)?
-//}
 
-//const selectingDrivers = [
-  //array of 2 elements: the two functions defined earlier
-  //returnFirstTwoDrivers()?
-  //returnLastTwoDrivers()?
-//]
+const selectingDrivers = [
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers
+]
 
-//const createFareMultiplier = high-order-function(integer) {
-  //return function of fare
-    //return fare multiplied by integer
-//}
+const createFareMultiplier = function(integer) {
+  return function (fare) {
+    return fare * integer
+  }
+}
 
-//const fareDoubler = createFareMultiplier(2)?
+const fareDoubler = createFareMultiplier(2)
 
-//const fareTripler = createFareMultiplier(3)?
+const fareTripler = createFareMultiplier(3)
 
-//function selectDifferentDrivers(drivers, (2 different functions, so function?)) {
-  //return either first2 or last2
-  //return the function, drivers of that function
-  //return function(drivers)?
-//}
+function selectDifferentDrivers(drivers, func) {
+  return func(drivers)
+}
+//keyword function was not passing this last test
