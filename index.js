c320997a0d8eb6d70e0332e1d6902da362b6a76a
Malind Kowalski <malind.nikulski@gmail.com>
2017-10-03 12:40:06 -0600

    pseudo code before testing

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+// const returnFirstTwoDrivers = anonymous function(arrayOfDrivers) {
+  // return driver1, driver2 (technically driver 0 & 1)
+    //maybe const twoDrivers = driver1 & driver2 selected
+    //return twoDrivers
+// }
+
+//const returnLastTwoDrivers = anonymous function(arrayOfDrivers) {
+  //return arrayOfDrivers.slice(-2)?
+//}
+
+//const selectingDrivers = [
+  //array of 2 elements: the two functions defined earlier
+  //returnFirstTwoDrivers()?
+  //returnLastTwoDrivers()?
+//]
+
+//const createFareMultiplier = high-order-function(integer) {
+  //return function of fare
+    //return fare multiplied by integer
+//}
+
+//const fareDoubler = createFareMultiplier(2)?
+
+//const fareTripler = createFareMultiplier(3)?
+
+//function selectDifferentDrivers(drivers, (2 different functions, so function?)) {
+  //return either first2 or last2
+  //return the function, drivers of that function
+  //return function(drivers)?
+//}
