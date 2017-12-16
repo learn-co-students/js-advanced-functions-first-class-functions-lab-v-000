d2d6341f922ee7dc502d61b1fc45b1e054335b09
lpassamano <leighayna.josele@gmail.com>
2017-12-16 17:38:54 -0500

    Done.

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 const returnFirstTwoDrivers = function (drivers) {
   return drivers.slice(0, 2);
-}
+};
 
 const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(-2 );
-}
+};
 
 const selectingDrivers = [
   returnFirstTwoDrivers,
@@ -16,12 +16,12 @@ const selectingDrivers = [
 function createFareMultiplier (integer) {
   return function (fare) {
     return fare * integer;
-  }
-}
+  };
+};
 
 const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
 function selectDifferentDrivers (drivers, fnctn) {
   return fnctn(drivers);
-}
+};
