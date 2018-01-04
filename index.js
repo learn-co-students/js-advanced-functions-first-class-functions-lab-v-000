17a916d924f5a85ff4d1c49eb21fcac72271a02e
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 20:02:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+  const copy = []
+  copy.push(drivers[0], drivers[1])
+  return copy
+}
+
+const returnLastTwoDrivers = function (drivers) {
+  const copy = []
+  copy.push(drivers[drivers.length - 2], drivers[drivers.length - 1])
+  return copy
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(mulitiplier) {
+  return function (fare) {
+    return fare * mulitiplier
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, cb) {
+  return cb(drivers)
+}
