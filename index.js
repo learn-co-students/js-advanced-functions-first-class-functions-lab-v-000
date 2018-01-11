4de4522aea468264de3f919098243574398f6456
dbfarms <matt.hunger@gmail.com>
2018-01-11 22:09:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (multiplier) {
+    return function (multiplier) {
+        return multiplier * multiplier
+    }
+}
+
+function fareDoubler(fare) {
+    return fare + fare
+}
+
+function fareTripler(fare) {
+    return fare * 3 
+}
+
+function selectDifferentDrivers(drivers, func){
+    return func(drivers)
+}
