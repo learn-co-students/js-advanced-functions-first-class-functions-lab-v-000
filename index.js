5b60b292699660e2c5e5a92b46818a8400f8efc6
Annette Maza <nettymaza@gmail.com>
2018-02-08 23:50:35 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+// Statement: is a unit of code that accomplishes something but does not produce a value.
+// Expression: is a unit of code that produces a value
+
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2)
+}
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(fareMultiplier) {
+  return function (num) {
+    return fareMultiplier * num
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(drivers, selectedDrivers) {
+  return selectedDrivers(drivers)
+}
