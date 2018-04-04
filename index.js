635bbed7d3e7135b557117e23bec195ba63d274f
Robert Laws <roblaws@me.com>
2018-04-04 19:04:26 +0300

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+}
+
+const selectingDrivers = [
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers
+]
+
+const createFareMultiplier = function(multiplier) {
+  return function() {
+    return multiplier * 5;
+  }
+}
+
+const fareDoubler = function(fare) {
+  return fare * 2;
+}
+
+const fareTripler = function(fare) {
+  return fare * 3;
+}
+
+const selectDifferentDrivers = function(arrayOfDrivers, myFunc) {
+  return myFunc(arrayOfDrivers);
+}
