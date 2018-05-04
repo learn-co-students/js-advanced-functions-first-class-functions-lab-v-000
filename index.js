8a26e7c6f19b20970ecc6dd8b96f653745480a6b
jacobsilver2 <jacobsilver2@mac.com>
2018-05-04 19:48:15 -0400

    work on step 5

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+// returnFirstTwoDrivers() — Declare a variable with const that is assigned an anonymous function. The assigned function should accept an array of drivers as an argument and return the first two drivers in the array.
+
+const returnFirstTwoDrivers = function (arr) { return arr.slice(0,2) }
+
+// returnLastTwoDrivers() — Declare a variable with const that is assigned an anonymous function. The assigned function should accept an array of drivers as an argument and return the last two drivers in the array.
+
+const returnLastTwoDrivers = function (arr) { return arr.slice(-2) };
+
+// selectingDrivers — This is an array containing two elements: the two functions that we previously defined (returnFirstTwoDrivers() and returnLastTwoDrivers()).
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// createFareMultiplier() — This is a higher-order function that takes in one argument, an integer, and returns a function that will multiply a fare for a ride accordingly. If createFareMultiplier() receives an argument of 4, it will return a function that takes in a fare as an argument and quadruples the fare.
+
+function createFareMultiplier(int) {
+  return function (fare) {return fare * int };
+}
+
+// fareDoubler() — Declare a variable with const and assign a function returned by createFareMultiplier() to it. Invoke createFareMultiplier() in such a way that the new fareDoubler() function accepts a fare as its lone argument and doubles it.
+
+const dude = 
