e2f40de6b265cde9390861a662d6a4cfe12f994b
github username <yosa.yon@gmail.com>
2018-05-24 22:06:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers){
+  return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+  return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//Higher-order function is a function that does at least one of the following: takes one or more functions as arguments (i.e. procedural parameters), returns a function as its result.in our case, it returns a function as a result! see below:
+
+const createFareMultiplier = function(multiple){
+  return function(fare){
+    return fare * multiple;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, fn){
+  return fn(drivers)
+}
