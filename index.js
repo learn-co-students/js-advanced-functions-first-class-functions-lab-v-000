98ccc5c2579650fdc3a79f324a17aebb1cc0ff8e
nmlstyl <joey.michalina.mariano@gmail.com>
2018-06-06 18:05:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers =
+  function returnFirstTwoDrivers(drivers){
+    let driversCopy = drivers.slice();
+    driversCopy.length = 2;
+    return driversCopy;
+  };
+
+const returnLastTwoDrivers =
+  function returnLastTwoDrivers(drivers){
+    debugger;
+    return drivers.slice(-2);
+  };
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier =
+  function createFareMultiplier(num){
+    return num * 5;
+  }
