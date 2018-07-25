af3b4f77df5b707cdee69c1b84ea4be36e0e722a
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-25 16:33:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(drivers) {
+  const firstTwo = function(drivers) {return [drivers[0], drivers[1]]};
+  return firstTwo();
+}
+
+function returnLastTwoDrivers(drivers) {
+  const lastTwo = function(drivers){ return [drivers[-2], drivers[-1]]};
+  return lastTwo();
+}
+
+function selectingDrivers() {
+  return [returnFirstTwoDrivers(), returnLastTwoDrivers()];
+}
+
+function createFareMultiplier(multiplier) {
+  return function(fare){ fare * multiplier};
+}
+
+function fareDoubler(fare) {
+  const doubler = createFareMultiplier(2)()
+  return doubler;
+}
