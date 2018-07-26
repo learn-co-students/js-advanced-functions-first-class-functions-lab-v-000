b69e7d0a4be93a2e38cabce1166cf6c098954f2f
Vanessa Toro <vanetoro84@gmail.com>
2018-07-26 19:07:31 +0000

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const returnFirstTwoDrivers = function(array){
 
 
 const returnLastTwoDrivers = function(array){
-  return [array[array.length-2], array[array.length-1]]
+  // return [array[array.length-2], array[array.length-1]]
+  return array.slice(-2)
 }
 
 const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
@@ -21,5 +22,12 @@ return multiply = function(fare){
 
 const fareDoubler = function(fare){
   return createFareMultiplier(2)(fare)
+}
 
+const fareTripler = function(fare){
+  return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(array, fn){
+  return fn(array)
 }
