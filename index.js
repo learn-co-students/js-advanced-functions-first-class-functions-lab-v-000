6a7683c5f3f73110b6f6db0a8fb909b1badfd9a5
AubreyNeimeier <neimeier.1@osu.edu>
2018-07-25 21:40:19 -0700

    first half of tests pass

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+function createFareMultiplier(integer){
+    return function (fare){
+        return integer * fare;
+    }
+}
+
