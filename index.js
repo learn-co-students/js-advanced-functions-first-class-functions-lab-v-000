2799c3489da122ddf19910f65e6a108b67c3b8bf
Katrina Brinson <kbrinso@gmail.com>
2018-08-28 19:47:10 +0000

    Still working

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (data){
+  return data.slice(0,2);
+}
+const returnLastTwoDrivers = function(data){
+  return data.slice(data.length - 2, data.length)
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(num){
+  return function(n){return n * n;};
+}
+
+function fareDoubler(fair){
+  const val = createFareMultiplier(fair);
+  return val;
+}
