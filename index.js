b0970928d002780696c9683b8b2d4d27c773805b
CSavi <carrielane89@gmail.com>
2018-08-31 12:48:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (driversArray){ 
+    const newArray = driversArray;
+    return newArray.slice(0, 2); 
+}
+
+const returnLastTwoDrivers = function (driversArray) {
+    const newArray = driversArray;
+    return newArray.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (num) { 
+    return (function (fare) {
+        return num * fare;
+    })
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (arrayOfDrivers, eitherDrivers) {
+    return eitherDrivers(arrayOfDrivers);
+}
+    
+
+
+
+    
