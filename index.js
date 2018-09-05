9121b927e52abdbf19ba67119676c5920c8f7446
Jul Jen <juliajen.prog@gmail.com>
2018-09-05 15:37:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+
+ // a variable with const that is assigned an anonymous function, assigned function accepts an array of drivers as an argument and returns the first two drivers in the array
+
+ const returnFirstTwoDrivers = function (nonDestructiveDrivers) {
+   return nonDestructiveDrivers.slice(0,2)
+ }
