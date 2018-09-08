8ecf81fef50d27d70c38035b35bee006d31eb6ea
John O'Connor <oconnojb@gmail.com>
2018-09-08 22:46:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) { return drivers.slice(0,2)}
+const returnLastTwoDrivers = function (drivers) { return drivers.slice(-2)}
+const selectingDrivers = [
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers
+]
+const createFareMultiplier = function (int) {return function (fare) {return fare * int }}
+const fareDoubler = function (fare) {return createFareMultiplier(2)(fare)}
+const fareTripler = function (fare) {return createFareMultiplier(3)(fare)}
+const fetchSpecifiedDrivers = function (drivers, fn) {return fn(drivers)}
+
+/*
+
++ `fetchSpecifiedDrivers()` — This function takes two arguments,
+an array of `drivers` and either the `returnFirstTwoDrivers()` or `returnLastTwoDrivers()` function.
+Based on these two arguments, `fetchSpecifiedDrivers()` will return either the first two drivers or the last two drivers.
+*/
