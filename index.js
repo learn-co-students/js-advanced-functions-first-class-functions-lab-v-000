388af238907be7210f40f8505eab405797eb983d
peterckim <peterckim96@gmail.com>
2018-09-17 14:57:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(drivers.length - 2);
+};
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// high order function that takes in one argument.
+function createFareMultiplier(multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function fetchSpecifiedDrivers(drivers, returnFirstTwoDrivers) {
+  return returnFirstTwoDrivers(drivers);
+}
