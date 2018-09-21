fa2bf0af6b9f3464495387b427a0b22888ba0938
roitblatari <roitblatari@gmail.com>
2018-09-21 11:34:09 -0400

    Done.

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const returnFirstTwoDrivers = function (drivers) {
   return drivers.slice(0,2);
 }
 
-const returnLastTwoDrivers = function (drivers) {
+const returnLastTwoDrivers = function (drivers) { //  return drivers.slice(-2); }
   return drivers.slice(drivers.length -2, drivers.length );
+  //  return drivers.slice(-2);
 }
 
 const selectingDrivers = [
@@ -14,7 +15,7 @@ const selectingDrivers = [
 
 function createFareMultiplier(num) {
   return function (num) {
-  return  num * num
+    return  num * num
   }
 }
 
@@ -22,9 +23,21 @@ function fareDoubler(num) {
   return  num + num;
 }
 
- function fareTripler(num) {
-  return fareDoubler(num) + num;
- }
+function fareTripler(num) {
+  return fareDoubler(num) + num ;
+}
+
+ // const createFareMultiplier = function (multiplyValue) {
+ //   return function (value) {
+ //     return multiplyValue * value;
+ //   };
+ // };
+ //
+ // const fareDoubler = createFareMultiplier(2);
+ //
+ // const fareTripler = createFareMultiplier(3);
+
+
 
 function fetchSpecifiedDrivers(arrayOfDrivers, cb) {
   return cb(arrayOfDrivers)
