5bce446e141cfb6322be8d439502c27e9d56628a
BrettSWeisberg <weisberg.brett@gmail.com>
2018-11-16 18:23:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+ function (drivers){
+   return drivers[0] + drivers[1];
+ }
+
+function returnFirstTwoDrivers() {
+  function (drivers){
+    return drivers[0] + drivers[1];
+}
