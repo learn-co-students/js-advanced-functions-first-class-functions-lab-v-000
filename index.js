b32ec83df059648d5997f22ed01c358998d5da0d
Kachel Wittig <kjwittig@gmail.com>
2018-12-23 00:49:36 -0600

    donezo

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(arr) {
+  return arr.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function (arr) {
+  return arr.slice(-2)
+}
+
+const selectingDrivers = [
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers
+]
+
+const createFareMultiplier = function (multiplier) {
+  return function (fare) {
+    return fare * multiplier
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const fetchSpecifiedDrivers = function (arr, fn) {
+   return fn(arr);
+}
