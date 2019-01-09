d88048968fea46860eca68a7e8abf0db0389a927
Greg Morgan <gregm9999@gmail.com>
2019-01-09 04:49:01 +0000

    Done.

--- index.js
+++ index.js
@@ -4,24 +4,20 @@ const returnFirstTwoDrivers = function(drivers) {
 }
 
 const returnLastTwoDrivers=function(drivers) {
-  return drivers.slice(-2, drivers.length);
+  return drivers.slice(-2);
 }
 
 const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers]
 
-const createFareMultiplier=function(fare) {
-    return function fareDo(fare) {
-      return fare*5;
+const createFareMultiplier=function(multiplyValue) {
+    return function (value) {
+      return multiplyValue * value;
     }
 }
 
-const fareDoubler=function(fare) {
-  return fare*2;
-}
+const fareDoubler=createFareMultiplier(2);
 
-const fareTripler=function(fare) {
-  return fare*3;
-}
+const fareTripler=createFareMultiplier(3);
 
 const fetchSpecifiedDrivers=function(drivers, functionName) {
   return functionName(drivers)
