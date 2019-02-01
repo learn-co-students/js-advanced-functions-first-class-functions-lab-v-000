05b32de866eb55be06a2ca25c61a255b8ddcaaea
freeWillee <will.ik.lee@gmail.com>
2019-02-01 12:07:11 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(collection) {
+        return collection.slice(0,2);
+    }
+
+const returnLastTwoDrivers = function(collection) {
+        return collection.slice(-2,collection.length);
+    }
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    }
+}
+
+// HOW DO THE FOLLOWING TWO TESTS WORK WITH CONSTANTS? 
+
+function fareDoubler(fare) {
+    return createFareMultiplier(2)(fare);
+}
+
+function fareTripler(fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+function fetchSpecifiedDrivers(arrayOfDrivers, fcn){
+    if (fcn === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(arrayOfDrivers);
+    } else {
+        return returnLastTwoDrivers(arrayOfDrivers);
+    };
+}
