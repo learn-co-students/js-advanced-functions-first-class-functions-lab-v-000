a79de197c8fa828eb32cd77d33b02bd184982912
Pamela Frieze <pfrieze@gmail.com>
2019-02-10 23:09:27 +0000

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ const returnFirstTwoDrivers = function(drivers) {
 const returnLastTwoDrivers = function(drivers) {
   return drivers.slice(Math.max(drivers.length - 2, 1))
 };
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+  return function(fare) {
+    return fare * integer
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function fetchSpecifiedDrivers(drivers, fnct) {
+  return fnct(drivers);
+}
+
