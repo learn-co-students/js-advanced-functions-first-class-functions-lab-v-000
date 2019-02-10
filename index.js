a5a79c244b96f8768d68fb392b4d2aa96444bcf1
Pamela Frieze <pfrieze@gmail.com>
2019-02-10 22:27:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+  };
+  
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(Math.max(drivers.length - 2, 1))
+};
