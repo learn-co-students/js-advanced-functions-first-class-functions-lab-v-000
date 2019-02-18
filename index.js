42b1315d9ab4f0d3b108c7370896b3753a89f2b5
daniellecroxton <danikrist@gmail.com>
2019-02-18 15:29:11 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0,2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const fetchSpecifiedDrivers = function(drivers, placement) {
+  return placement(drivers);
+};
+
+//
+// var friends = [“Mike”, “Stacy”, “Andy”, “Rick”];
+//
+// friends.forEach(function (eachName, index){
+// console.log(index + 1 + “. ” + eachName); // 1. Mike, 2. Stacy, 3. Andy, 4. Rick
+// });
