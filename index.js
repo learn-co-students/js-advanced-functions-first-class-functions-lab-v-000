6b9727e7b65f5cd93b6619923a96e11095e8ce0d
Guess <guess@Guesss-iMac.local>
2019-05-09 17:19:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array) {
+  return array.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(array) {
+  return array.slice(array.length - 2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer){
+  return function(i){return integer * i}
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function fetchSpecifiedDrivers(array, someFunction) {
+  return someFunction(array)
+}
