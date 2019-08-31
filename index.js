e7544ad68905f51987d2dbbd9e056f9bab8dbb94
Jason Mei <jason.mei403@gmail.com>
2019-08-30 23:42:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [
+  returnFirstTwoDrivers(),
+  returnLastTwoDrivers()
+];
