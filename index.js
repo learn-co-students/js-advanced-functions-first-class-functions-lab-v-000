abf4ce133dbb4c52e7244847f0b4514110869dab
github username <constance.hyman@mchsi.com>
2019-09-24 21:51:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(arr){
+    return arr.slice(0,2);
+  }
+
+
+const returnLastTwoDrivers = function(arr){
+    return arr.slice(-2);
+  }
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int){
+  return function(fare){
+    return fare * int
+  };
+}
+
+function fareDoubler(fare){
+    return fare * 2
+  };
+
+function fareTripler(fare){
+    return fare * 3
+  };
+
+function selectDifferentDrivers(collection, subsetFunction){
+  return subsetFunction(collection);
+};
