12e887ab3ec8247f88e42c48623b21cb018bdfe7
Rebecca Tong <rtong@buffalo.edu>
2019-11-19 15:07:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+// returnFirstTwoDrivers() — Declare a variable with const that is assigned an anonymous function.
+// The assigned function should accept an array of drivers as an argument and return
+// // the first two drivers in the array.
+const returnFirstTwoDrivers = function(arr){
+  return arr.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(arr){
+  return arr.pop(1,arr.length)
+}
