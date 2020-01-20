c0940bfbe8ffc7c06abdd1698fc2281330fa0a42
Meg Gutshall <meghan.gutshall@gmail.com>
2020-01-20 15:23:53 -0500

    Done.

--- index.js
+++ index.js
@@ -9,13 +9,27 @@ const returnLastTwoDrivers = function(drivers) {
 let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
 function createFareMultiplier(multiple) {
-  return function(fare) {
+  return function multiply(fare) {
     return fare * multiple;
   };
 };
 
-const fareDoubler = function createFareMultiplier(multiple = 2) {
-  return function(fare) {
-    return fare * multiple;
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, selectingDrivers) {
+  function shuffle(array1) {
+    var ctr = array1.length, temp, index;
+    while (ctr > 0) {
+      index = Math.floor(Math.random() * ctr);
+      ctr--;
+      temp = array1[ctr];
+      array1[ctr] = array1[index];
+      array1[index] = temp;
+    }
+    return array1;
   };
+  let rand = shuffle(selectingDrivers);
+  return rand(drivers);
 };
