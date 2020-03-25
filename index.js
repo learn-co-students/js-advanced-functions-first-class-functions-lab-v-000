91e702ac20a257c7f9fa9cc4f7f8bc3d206269c3
Blake A. Long <blake.a.long@gmail.com>
2020-03-25 14:34:23 -0600

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2)
 
-
 const returnLastTwoDrivers = (drivers) => drivers.slice(-2)
-
 
 const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ]
 
