37e65f303b73165cc01448a0c632d38b78f1357e
Pat  Pignataro <pat6284@msn.com>
2020-09-11 13:33:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const returnFirstTwoDrivers = function(drivers) {
-  return drivers.slice(0, 2);
+const returnFirstTwoDrivers = function(driver_array) {
+  return driver_array.slice(0,2)
 }
 
-const returnLastTwoDrivers = function(drivers) {
-  return drivers.slice(-2)
+const returnLastTwoDrivers = function(driver_array) {
+  return driver_array.slice(-2)
 }
 
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-function createFareMultiplier(fare) {
-  return function(num) {
-    return fare * num;
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+  return function(fare) {
+    return multiplier * fare
   }
 }
 
-const multiplier = createFareMultiplier(2);
-function fareDoubler(fare) {
-  return multiplier(fare);
+const fareDoubler = function(fare) {
+  return createFareMultiplier(2)(fare)
 }
 
-const tripler = createFareMultiplier(3);
-function fareTripler(fare) {
-  return tripler(fare);
+const fareTripler = function(fare) {
+  return createFareMultiplier(3)(fare)
 }
 
-function selectDifferentDrivers(drivers, cb) {
-  return cb(drivers);
+function selectDifferentDrivers(driver_array, driver_function) {
+  return driver_function(driver_array)
 }
