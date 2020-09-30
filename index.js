ee20bb1b05804d76ccc9bc7f833aad8be726180e
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-09-30 23:07:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 // Code your solution in this file!
 
 function returnFirstTwoDrivers(arrayOfDrivers){
-  let newArray = [arrayOfDrivers[0], arrayOfDrivers[1]];
-  return newArray;
+  // let newArray = [arrayOfDrivers[0], arrayOfDrivers[1]];
+  // return newArray;
+  return arrayOfDrivers.slice(0, 2)
 }
 
 function returnLastTwoDrivers(arrayOfDrivers){
-  let index = arrayOfDrivers.length;
-  let newArray = [arrayOfDrivers[index - 2], arrayOfDrivers[index - 1]];
-  return newArray;
+  // let index = arrayOfDrivers.length;
+  // let newArray = [arrayOfDrivers[index - 2], arrayOfDrivers[index - 1]];
+  // return newArray;
+     return arrayOfDrivers.slice(-2)
 }
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-const createFareMultiplier = (a) => (b) => { return a * b };
+const createFareMultiplier = (a) => (b) => { return a * b }; //ES6 notation
 
-const fareDoubler = (a) => { return a * 2 };
+const fareDoubler = createFareMultiplier(2);
+// same as above
+// const fareDoubler = (a) => { return a * 2 };
 
-const fareTripler = (a) => { return a * 3 };
+const fareTripler = createFareMultiplier(3);
+// same as above
+// const fareTripler = (a) => { return a * 3 };
 
-function selectDifferentDrivers(drivers, func) {
-  return func(drivers);
+function selectDifferentDrivers(drivers, selection) {
+  return selection(drivers);
 }
