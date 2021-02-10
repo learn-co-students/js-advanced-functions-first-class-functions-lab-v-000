0d8a9dfd88a97174b3b337791582b5233c804243
Liz Burton <liz.burton147@gmail.com>
2021-02-10 10:41:25 -0500

    Small edit for clarity

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-const selectDifferentDrivers = function (drivers, whichDrivers) {
-  return whichDrivers(drivers);
+const selectDifferentDrivers = function (drivers, driversToReturn) {
+  return driversToReturn(drivers);
 };
