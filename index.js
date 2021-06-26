3b4baa2c08af5e350d4498262586b8141c2de6fd
halfields <hfields697@aol.com>
2021-06-25 20:42:37 -0500

    done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const returnFirstTwoDrivers = function (drivers) {
 }
 
 const returnLastTwoDrivers = function (drivers) {
-	return drivers.slice(2);
+	return drivers.slice(-2);
 }
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
