17389d1bd19964e449a5d6538b13ca61e0cd581d
Dani <dani8439@aol.com>
2021-10-08 14:28:27 -0400

    Working out Shopify Challenge for prosperity.

--- index.js
+++ index.js
@@ -1,25 +1,106 @@
 // Code your solution in this file!
 
-const returnFirstTwoDrivers = function(drivers) {
+const returnFirstTwoDrivers = function (drivers) {
   return drivers.slice(0, 2);
-}
+};
 
-const returnLastTwoDrivers = function(drivers) {
+const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(-2);
-}
+};
 
-const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers];
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-const createFareMultiplier = function(multiplyValue) {
-  return function(value) {
+const createFareMultiplier = function (multiplyValue) {
+  return function (value) {
     return multiplyValue * value;
-  }
-}
+  };
+};
 
 const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-const selectDifferentDrivers = function(drivers, whichDrivers) {
+const selectDifferentDrivers = function (drivers, whichDrivers) {
   return whichDrivers(drivers);
-}
+};
+
+////// Shopify Challenge
+const fareCalculator = function (arr) {
+  const stations = ["Atlantic", "Pacific", "Indian", "Arctic", "Southern"];
+  const desertLine = [
+    "Sahara",
+    "Pacific",
+    "Gobi",
+    "Kalahari",
+    "Mojave",
+    "Namib",
+  ];
+  // let fare = 1.5;
+  let total = arr[3];
+  let origin = arr[1];
+  let destination = arr[2];
+  // console.log(total);
+
+  let fare =
+    Math.abs(stations.indexOf(destination) - stations.indexOf(origin)) * 0.5 +
+    1.0;
+  let newBalance = total - fare;
+
+  return [fare, newBalance];
+};
+
+fareCalculator([123, "Atlantic", "Pacific", 50.0]);
+fareCalculator([1, "Atlantic", "Pacific", 22.5]);
+fareCalculator([2, "Atlantic", "Pacific", 1.0]);
+fareCalculator([3, "Pacific", "Atlantic", 3.0]);
+fareCalculator([3, "Atlantic", "Pacific", 1.5]);
+fareCalculator([3, "Pacific", "Southern", 3.0]);
+fareCalculator([1, "Atlantic", "Indian", 50.0]);
+fareCalculator([2, "Atlantic", "Southern", 2.0]);
+fareCalculator([3, "Southern", "Atlantic", 0.5]);
+
+// def calculate_fare(tap_out)
+//   balance = tap_out[3]
+//   origin = tap_out[1]
+//   destination = tap_out[2]
+
+//   stops = ["Atlantic", "Pacific", "Indian", "Arctic", "Southern"]
+//   fare = ((stops.index(destination) - stops.index(origin)).abs * 0.50) + 1.00
+
+//   new_balance = balance - fare
+
+//   return [fare , new_balance]
+
+// end
+
+// ["Atlantic", "Pacific", "Indian", "Arctic", "Southern"]
+
+// input = [123, "Atlantic", "Pacific", 50.00]
+// output = [1.50, 48.50]
+
+// raise if calculate_fare(input) != output
+
+// input = [1, "Atlantic", "Pacific", 22.50]
+// output = [1.50, 21.00]
+
+// raise if calculate_fare(input) != output
+
+// input = [2, "Atlantic", "Pacific", 1.00]
+// output = [1.50, -0.50]
+
+// raise if calculate_fare(input) != output
+
+// input = [3, "Pacific", "Atlantic", 3.00]
+// output = [1.50, 1.50]
+
+// raise if calculate_fare(input) != output
+
+// input = [3, "Atlantic", "Pacific", 1.50]
+// output = [1.50, 0.00]
+
+// raise if calculate_fare(input) != output
+
+// input = [3, "Pacific", "Southern", 3.00]
+// output = [2.50, 0.50]
+
+// raise if calculate_fare(input) != output
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,79 +1,150 @@
-describe('index.js', function () {
-  const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+const { expect } = require("chai");
+
+describe("index.js", function () {
+  const drivers = ["Sally", "Bob", "Freddy", "Claudia"];
 
   afterEach(function () {
-    expect(drivers, 'MAKE SURE YOUR ARRAY MANIPULATIONS ARE NON-DESTRUCTIVE').to.eql(['Sally', 'Bob', 'Freddy', 'Claudia']);
+    expect(
+      drivers,
+      "MAKE SURE YOUR ARRAY MANIPULATIONS ARE NON-DESTRUCTIVE"
+    ).to.eql(["Sally", "Bob", "Freddy", "Claudia"]);
   });
 
-  describe('returnFirstTwoDrivers()', function () {
-    it('should return a new array containing the first two drivers from the passed-in array', function () {
-      expect(returnFirstTwoDrivers(drivers)).to.eql(['Sally', 'Bob']);
+  describe("returnFirstTwoDrivers()", function () {
+    it("should return a new array containing the first two drivers from the passed-in array", function () {
+      expect(returnFirstTwoDrivers(drivers)).to.eql(["Sally", "Bob"]);
     });
   });
 
-  describe('returnLastTwoDrivers()', function () {
-    it('should return an array of the last two drivers', function () {
-      expect(returnLastTwoDrivers(['Sally', 'Bob', 'Freddy', 'Claudia'])).to.eql(['Freddy', 'Claudia']);
+  describe("returnLastTwoDrivers()", function () {
+    it("should return an array of the last two drivers", function () {
+      expect(
+        returnLastTwoDrivers(["Sally", "Bob", "Freddy", "Claudia"])
+      ).to.eql(["Freddy", "Claudia"]);
     });
   });
 
-  describe('selectingDrivers', function () {
-    it('has the `returnFirstTwoDrivers` function to as its first element', function () {
+  describe("selectingDrivers", function () {
+    it("has the `returnFirstTwoDrivers` function to as its first element", function () {
       expect(selectingDrivers[0]).to.eql(returnFirstTwoDrivers);
     });
 
-    it('has the `returnLastTwoDrivers` function to as its last element', function () {
+    it("has the `returnLastTwoDrivers` function to as its last element", function () {
       expect(selectingDrivers[1]).to.eql(returnLastTwoDrivers);
     });
 
-    it('allows us to invoke either function from the array', function () {
-      expect(selectingDrivers[0](drivers)).to.eql(['Sally', 'Bob']);
+    it("allows us to invoke either function from the array", function () {
+      expect(selectingDrivers[0](drivers)).to.eql(["Sally", "Bob"]);
 
-      expect(selectingDrivers[1](drivers)).to.eql(['Freddy', 'Claudia']);
+      expect(selectingDrivers[1](drivers)).to.eql(["Freddy", "Claudia"]);
     });
   });
 
-  describe('createFareMultiplier()', function () {
-    it('returns a function', function () {
+  describe("createFareMultiplier()", function () {
+    it("returns a function", function () {
       const fareMultiplier = createFareMultiplier(2);
 
-      expect(fareMultiplier).to.be.a('function');
+      expect(fareMultiplier).to.be.a("function");
     });
 
-    it('should multiply a given value using the created multiplier', function () {
+    it("should multiply a given value using the created multiplier", function () {
       const fareQuintupler = createFareMultiplier(5);
 
       expect(fareQuintupler(5)).to.eql(25);
     });
   });
 
-  describe('fareDoubler()', function () {
-    it('is a function', function () {
-      expect(fareDoubler).to.be.a('function');
+  describe("fareDoubler()", function () {
+    it("is a function", function () {
+      expect(fareDoubler).to.be.a("function");
     });
 
-    it('doubles fares', function () {
+    it("doubles fares", function () {
       expect(fareDoubler(10)).to.eql(20);
     });
   });
 
-  describe('fareTripler()', function () {
-    it('is a function', function () {
-      expect(fareTripler).to.be.a('function');
+  describe("fareTripler()", function () {
+    it("is a function", function () {
+      expect(fareTripler).to.be.a("function");
     });
 
-    it('triples fares', function() {
+    it("triples fares", function () {
       expect(fareTripler(12)).to.eql(36);
     });
   });
 
-  describe('selectDifferentDrivers(arrayOfDrivers, function)', function () {
-    it('returns the first two drivers when passed returnFirstTwoDrivers() as the second argument', function () {
-      expect(selectDifferentDrivers(drivers, returnFirstTwoDrivers)).to.eql(['Sally', 'Bob']);
+  describe("selectDifferentDrivers(arrayOfDrivers, function)", function () {
+    it("returns the first two drivers when passed returnFirstTwoDrivers() as the second argument", function () {
+      expect(selectDifferentDrivers(drivers, returnFirstTwoDrivers)).to.eql([
+        "Sally",
+        "Bob",
+      ]);
     });
 
-    it('returns the last two drivers when passed returnLastTwoDrivers() as the second argument', function () {
-      expect(selectDifferentDrivers(drivers, returnLastTwoDrivers)).to.eql(['Freddy', 'Claudia']);
+    it("returns the last two drivers when passed returnLastTwoDrivers() as the second argument", function () {
+      expect(selectDifferentDrivers(drivers, returnLastTwoDrivers)).to.eql([
+        "Freddy",
+        "Claudia",
+      ]);
+    });
+  });
+
+  describe("fareCalculator()", function () {
+    it("is a function", function () {
+      expect(fareCalculator).to.be.a("function");
+    });
+
+    it("calculates fares", function () {
+      expect(fareCalculator([123, "Atlantic", "Pacific", 50.0])).to.eql([
+        1.5, 48.5,
+      ]);
+    });
+
+    it("calculates fares", function () {
+      expect(fareCalculator([1, "Atlantic", "Pacific", 22.5])).to.eql([
+        1.5, 21.0,
+      ]);
+    });
+
+    it("calculates fares", function () {
+      expect(fareCalculator([2, "Atlantic", "Pacific", 1.0])).to.eql([
+        1.5, -0.5,
+      ]);
+    });
+
+    it("calculates fares", function () {
+      expect(fareCalculator([3, "Pacific", "Atlantic", 3.0])).to.eql([
+        1.5, 1.5,
+      ]);
+    });
+
+    it("calculates fares", function () {
+      expect(fareCalculator([3, "Atlantic", "Pacific", 1.5])).to.eql([1.5, 0]);
+    });
+
+    it("calculates fares", function () {
+      expect(fareCalculator([3, "Pacific", "Southern", 3.0])).to.eql([
+        2.5, 0.5,
+      ]);
+    });
+
+    it("calculates fares", function () {
+      expect(fareCalculator([1, "Atlantic", "Indian", 50.0])).to.eql([
+        2.0, 48.0,
+      ]);
+    });
+
+    it("calculates fares", function () {
+      expect(fareCalculator([2, "Atlantic", "Southern", 2.0])).to.eql([
+        3.0, -1.0,
+      ]);
+    });
+
+    it("calculates fares", function () {
+      expect(fareCalculator([3, "Southern", "Atlantic", 0.5])).to.eql([
+        3.0, -2.5,
+      ]);
     });
   });
 });
